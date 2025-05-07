c1df8259c462dc455fdd71fbe8ed7d65e2c565ee
rana <rana.buddy@gmail.com>
2025-05-07 11:22:04 +0600

    feat:first draft

---
+++ database.js
@@ -0,0 +1,18 @@
+import mongoose  from 'mongoose'
+
+const mongouri='mongodb+srv://monamak426:<db_password>@database.umouihz.mongodb.net/meetdatabase?retryWrites=true&w=majority&appName=database'
+
+// const mongouri ='mongodb://mikejson7856:Xo29r4BIdGff5nFQ@ac-2mrpor8-shard-00-00.yqadcrk.mongodb.net:27017,ac-2mrpor8-shard-00-01.yqadcrk.mongodb.net:27017,ac-2mrpor8-shard-00-02.yqadcrk.mongodb.net:27017/skipdatabase?ssl=true&replicaSet=atlas-tx134a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+const connectDB = () => {
+
+    mongoose.connect(mongouri
+    ).then((result) => {
+        console.log('mongo connected');
+    })
+        .catch((err) => { console.log(err) });
+}
+
+export default connectDB
+
+
+
