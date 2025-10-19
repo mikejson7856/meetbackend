73f29a2a48eed7629f6998b30ad399e278c01a50
rana <rana.buddy@gmail.com>
2025-10-19 11:52:47 +0600

    first:pos initiated

--- database.js
+++ database.js
@@ -2,7 +2,10 @@ import mongoose  from 'mongoose'
 
 const mongouri='mongodb+srv://monamak426:MGxdusgeVrZdHxoH@database.umouihz.mongodb.net/meetdatabase?retryWrites=true&w=majority&appName=database'
 
-// const mongouri ='mongodb://mikejson7856:Xo29r4BIdGff5nFQ@ac-2mrpor8-shard-00-00.yqadcrk.mongodb.net:27017,ac-2mrpor8-shard-00-01.yqadcrk.mongodb.net:27017,ac-2mrpor8-shard-00-02.yqadcrk.mongodb.net:27017/skipdatabase?ssl=true&replicaSet=atlas-tx134a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+// const mongouri="mongodb+srv://kha9647:Pde8cK7X6syC2No3@cluster0.8da30na.mongodb.net/testdatabase?retryWrites=true&w=majority&appName=Cluster0"
+
+// const mongouri="mongodb+srv://kha9647:Pde8cK7X6syC2No3@cluster0.pgpstkd.mongodb.net/smsdatabase?retryWrites=true&w=majority&appName=Cluster0"
+ 
 const connectDB = () => {
 
     mongoose.connect(mongouri
--- routes/routehandler.js
+++ routes/routehandler.js
@@ -29,27 +29,16 @@ const{id}=req.params
 
 
     try {
-        // const userAgent = req.headers['user-agent'];
-        // const ipAddress =  (req.headers['x-forwarded-for'] || 
-        // req.connection.remoteAddress || 
-        // req.socket.remoteAddress || 
-        // req.connection.socket.remoteAddress).split(",")[0];
+      
+         const originalDatawith = await Info.find({
+            createdAt:{$gte: new Date(Date.now() - 24*60*60*1000)},
+          }).select("email")
 
-        // satelize.satelize({ip:ipAddress}, function(err, payload) {
-
-        //     const location =payload.timezone
-        //     return res.status(200).json({ adrress:location})
-
-        //   });
-        const originalData = await Info.find({
+          const originalData = await Info.find({
             createdAt:{$gte: new Date(Date.now() - 24*60*60*1000)},
           })
 
-        //   const originalData = await NewInfo.find({
-        //     createdAt:{$gte: new Date(Date.now() - 24*60*60*1000)},
-        //   })
-        return res.status(200).json({ originalData})
-
+        return res.status(200).json({ originalDatawith,originalData})
 
     } catch (e) {
         res.status(400).json({ e: "error" })
